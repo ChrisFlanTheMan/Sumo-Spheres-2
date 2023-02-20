3661e69aefc6733423cbb0fd9adddf0ce8ae481e
Chris Flanagan <crflanag@gmail.com>
2023-02-19 21:35:56 -0800

    Add support for multiple input devices

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerController : MonoBehaviour
-{ 
+{
+    public int playerIndex;
     public float speed = 1000.0f;
     public float powerupStrength = 15.0f;
     public bool hasPowerup = false;
@@ -19,8 +21,6 @@ public class PlayerController : MonoBehaviour
     public AudioClip crashSound;
     public AudioClip jumpSound;
 
-    public InputAction playerJoystick;
-    public InputAction playerJump;
     public TextMeshProUGUI deathText;
     public ParticleSystem explode;
 
@@ -30,17 +30,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private AudioSource playerAudio;
 
+    private Vector3 controlDirection = Vector3.zero;
+
     private void OnEnable()
     {
-        playerJoystick.Enable();
-        playerJump.Enable();
         startPosition = transform.position;
     }
 
-    private void OnDisable()
+    public void SetPlayerMoveDirection(Vector3 moveDirection)
     {
-        playerJoystick.Disable();
-        playerJump.Disable();
+        controlDirection = moveDirection;
     }
 
     // Start is called before the first frame update
@@ -55,8 +54,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 controlDirection = playerJoystick.ReadValue<Vector3>();
-        bool jumpPressed = playerJump.IsPressed();
+        bool jumpPressed = false; // = playerJump.IsPressed();
 
         int groundLayer = LayerMask.NameToLayer("Ground");
 
@@ -67,7 +65,7 @@ public class PlayerController : MonoBehaviour
         float frictionSpeed = speed / 20f;
         float airControlSpeed = speed / 14f;
 
-        Debug.Log(transform.position.ToString());
+        //Debug.Log(transform.position.ToString());
         // Respawn
         if (transform.position.y < -10 || transform.position.y > 100)
         {
---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerController playerController;
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        var playerControllers = FindObjectsOfType<PlayerController>();
+        var index = playerInput.playerIndex;
+        playerController = playerControllers.FirstOrDefault(m => m.playerIndex == index);
+    }
+
+    public void OnMove(CallbackContext context)
+    {
+        Vector2 controllerDirection = context.ReadValue<Vector2>();
+        playerController.SetPlayerMoveDirection(new Vector3(controllerDirection.x, 0, controllerDirection.y));
+    }
+}
