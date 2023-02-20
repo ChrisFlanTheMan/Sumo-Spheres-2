48227ca6fb6820adb9410b4d87bd2c95c0e4d3fd
paulamaii <paula.prz@live.com>
2023-02-19 18:16:21 -0800

    World Rotation and Bloom

---
+++ Assets/RotateSkyDome.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkyDome : MonoBehaviour
+{
+    private float skyRotationSpeed = 0;
+    public float rotationFallOff = 15;//how much the island speed is divided by for the sky speed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        skyRotationSpeed = GetComponent<Rotate>().worldRotationSpeed; 
+        skyRotationSpeed = -skyRotationSpeed/rotationFallOff;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * skyRotationSpeed);
+    }
+}
